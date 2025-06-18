f7c9172e2ef25a9e688c1c4104ed6b75e7b19baa
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-06-18 15:32:03 +0100

    updating

--- frontend/src/app/components/ModeSwiper.tsx
+++ frontend/src/app/components/ModeSwiper.tsx
@@ -55,11 +55,9 @@ export default function ModeSwiper()
                 />
               </div>
               <div className={styles.textContainer}>
-                <h2 className={styles.title}>{mode.title}</h2>
-                <p className={styles.description}>{mode.description}</p>
-                <button className={styles.button}  onClick={() => handleClick(mode.route)}>
-                  <h2>{mode.buttonname}</h2>
-                </button>
+                  <button className={styles.button}  onClick={() => handleClick(mode.route)}>
+                    {mode.buttonname}
+                  </button>
               </div>
             </SwiperSlide>
           ))}
