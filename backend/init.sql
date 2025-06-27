32f66e46516ea108056b55b3fb7c41aa99487779
Abdelmoughit Echcharbiny <abechcha@e1r9p7.1337.ma>
2025-06-27 18:27:08 +0100

    fetch message error

--- backend/init.sql
+++ backend/init.sql
@@ -13,9 +13,9 @@ CREATE TABLE users (
     langue TEXT,
     status TEXT,
     google_auth TEXT,
-    friends TEXT, -- Storing as JSON array
-    friends_request TEXT, -- Storing as JSON array
-    blocked_users TEXT -- Storing as JSON array
+    friends TEXT,
+    friends_request TEXT,
+    blocked_users TEXT
 );
 
 CREATE TABLE game_history (
@@ -40,7 +40,7 @@ CREATE TABLE achievement (
 CREATE TABLE room (
     conversation_id INTEGER PRIMARY KEY AUTOINCREMENT,
     getter_user INTEGER NOT NULL,
-    members TEXT NOT NULL, -- Storing as JSON array
+    members TEXT NOT NULL,
     room_name TEXT,
     iconPad TEXT,
     FOREIGN KEY (getter_user) REFERENCES users(id_user)
@@ -71,9 +71,49 @@ CREATE TABLE message (
 );
 
 
-INSERT INTO users (username, fullname, email, password) 
-VALUES ('zalksya', 'Zalksya User', 'zalksya@example.com', 'hashed_password_123');
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('zalksya', 'Zalksya User', 'https://cdn-icons-png.flaticon.com/512/6858/6858504.png','zalksya@example.com', 'hashed_password_123');
 
 
-INSERT INTO users (username, fullname, email, password) 
-VALUES ('younes', 'Younes User', 'younes@example.com', 'hashed_password_456');
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('younes', 'Younes User', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCMDKvDLrPdTJtG5O4y3W61Wdqg20GwOOpUA&s' ,'younes@example.com', 'hashed_password_456');
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('aaaaaaaaa', 'aaaaaaaa User', 'https://img.freepik.com/premium-vector/male-face-avatar-icon-set-flat-design-social-media-profiles_1281173-3806.jpg?semt=ais_hybrid&w=740' ,'aaaaaaaaaaaaaaaaaaaaaaaa@example.com', 'hashed_password_456');
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test1', 'test User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teset@example.com', 'hashed_password_456');
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test2', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teedst@example.com', 'hashed_password_456');
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test3', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tedst@example.com', 'hashed_password_456');
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test4', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tesast@example.com', 'hashed_password_456');
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test5', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teacst@example.com', 'hashed_password_456');
+
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test6', 'test  User', 'https://cdn.intra.42.fr/users/9ae5b3303aaceb68d7a6e580c60545a4/yzoullik.jpg' ,'tesact@example.com', 'hashed_password_456');
+
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test7', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tes zxt@example.com', 'hashed_password_456');
+
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test8', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teszxt@example.com', 'hashed_password_456');
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test9', 'test User', 'https://cdn.intra.42.fr/users/806ad2f231069b86aff6a24a34070d1b/zalaksya.jpg' ,'tasest@example.com', 'hashed_password_456');
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('test12', 'test User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'test@eqexample.com', 'hashed_password_456');
+
+
+INSERT INTO users (username, fullname, profile_img ,email, password) 
+VALUES ('tesdt12', 'tedst User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tesdt@eqexample.com', 'hashed_password_456');
--- backend/modules/user.module.js
+++ backend/modules/user.module.js
@@ -1,3 +1,5 @@
+import fastify from "fastify";
+
 export async function createUser(request, reply) {
   return {
     message: `User created`,
@@ -10,4 +12,23 @@ export async function getUsers(request, reply) {
   return {
     message: `User hhhhhhh khdam `,
   };
+}
+
+
+export async function aaa(request, reply) {
+  try {
+    const users = request.server.db.prepare("SELECT * FROM users").all();
+    reply.send(users);
+  } catch (err) {
+    reply.code(500).send({ error: 'Database query failed' });
+  }
+}
+
+
+export async function getConversation(request , reply){
+ console.log('Route params:', request.body);
+  reply.send([
+    {id: 1 , ms:'hello brother are you fine ', sender:'avatar'},
+    {id: 2 , ms:'hello brother are you fine ', sender:'savatar'}
+]);
 }
