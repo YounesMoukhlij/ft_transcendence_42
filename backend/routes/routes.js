7b37e0861980899b9d647ad2c79e48b1945e8d33
Abdelmoughit Echcharbiny <abechcha@e1r9p7.1337.ma>
2025-06-27 18:27:08 +0100

    fetch message error

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,9 @@
-import { createUser, getUsers } from '../modules/user.module.js';
+import { createUser, getUsers  , getConversation ,aaa} from '../modules/user.module.js';
 
 export default async function routes(fastify, options) {
   fastify.get('/user', createUser);
   fastify.get('/users', getUsers);
+  fastify.get('/', aaa);
+  fastify.post('/getConversation', getConversation);
+
 }
--- backend/server.js
+++ backend/server.js
@@ -4,47 +4,43 @@ import Database from "better-sqlite3";
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from '@fastify/cors'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = fastify({ logger: true });
+const app = fastify();
 const db = new Database('Database.db');
 
-try {
+app.register(cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
 
+
+app.decorate('db', db);
+app.register(routes);
+
+try {
   const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
   if (tables.length === 0) {
     const init = fs.readFileSync(path.join(__dirname, 'init.sql'), 'utf8');
     db.exec(init);
-    app.log.info('Database initialized');
+    app.log.info('database initialized');
   }
 } catch (err) {
-  app.log.error('Database initialization failed:', err);
+  app.log.error('database initialization error:', err);
   process.exit(1);
 }
 
-app.decorate('db', db);
 
 
-app.register(routes);
-
-app.get('/', async (request, reply) => {
-  try {
-    const stmt = db.prepare("SELECT * FROM users");
-    const users = stmt.all(); 
-    return users;
-  } catch (err) {
-    app.log.error(err);
-    reply.code(500).send({ error: 'Database query failed' });
-  }
-});
 
 
 const start = async () => {
   try {
-    await app.listen({ port: 4000 });
-    console.log('Server running at http://localhost:4000');
+    await app.listen({ port: 4444 });
+    console.log('Server running at http://localhost:4444');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
