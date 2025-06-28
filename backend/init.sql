662122e20b9c603cf9f5a6ff2a3bdd5557736585
mrmoughit <xblackhacker1@gmail.com>
2025-06-29 00:00:16 +0100

    save messages at db

--- backend/init.sql
+++ backend/init.sql
@@ -39,12 +39,11 @@ CREATE TABLE achievement (
 
 CREATE TABLE room (
     conversation_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    getter_user INTEGER NOT NULL,
     members TEXT NOT NULL,
     room_name TEXT,
-    iconPad TEXT,
-    FOREIGN KEY (getter_user) REFERENCES users(id_user)
+    iconPad TEXT
 );
+
 
 
 CREATE TABLE notification (
@@ -64,11 +63,11 @@ CREATE TABLE message (
     conv_id INTEGER NOT NULL,
     message TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    sender INTEGER NOT NULL,
+    sender TEXT NOT NULL,
     isSeen BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (conv_id) REFERENCES room(conversation_id),
-    FOREIGN KEY (sender) REFERENCES users(id_user)
+    FOREIGN KEY (conv_id) REFERENCES room(conversation_id)
 );
+
 
 
 INSERT INTO users (username, fullname, profile_img ,email, password) 
@@ -81,39 +80,48 @@ VALUES ('younes', 'Younes User', 'https://encrypted-tbn0.gstatic.com/images?q=tb
 INSERT INTO users (username, fullname, profile_img ,email, password) 
 VALUES ('aaaaaaaaa', 'aaaaaaaa User', 'https://img.freepik.com/premium-vector/male-face-avatar-icon-set-flat-design-social-media-profiles_1281173-3806.jpg?semt=ais_hybrid&w=740' ,'aaaaaaaaaaaaaaaaaaaaaaaa@example.com', 'hashed_password_456');
 
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test1', 'test User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teset@example.com', 'hashed_password_456');
 
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test2', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teedst@example.com', 'hashed_password_456');
-
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test3', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tedst@example.com', 'hashed_password_456');
-
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test4', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tesast@example.com', 'hashed_password_456');
-
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test5', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teacst@example.com', 'hashed_password_456');
+INSERT INTO room (members, room_name, iconPad)
+VALUES (',abechcha,zalksya,', 'Friends Chat', 'default_icon.png');
 
 
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test6', 'test  User', 'https://cdn.intra.42.fr/users/9ae5b3303aaceb68d7a6e580c60545a4/yzoullik.jpg' ,'tesact@example.com', 'hashed_password_456');
+INSERT INTO message (conv_id, message, sender)
+VALUES (1, 'Hello, how are you iam zakariya ?', 'Zalksya');
 
 
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test7', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tes zxt@example.com', 'hashed_password_456');
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test1', 'test User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teset@example.com', 'hashed_password_456');
+
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test2', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teedst@example.com', 'hashed_password_456');
+
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test3', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tedst@example.com', 'hashed_password_456');
+
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test4', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tesast@example.com', 'hashed_password_456');
+
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test5', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teacst@example.com', 'hashed_password_456');
 
 
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test8', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teszxt@example.com', 'hashed_password_456');
-
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test9', 'test User', 'https://cdn.intra.42.fr/users/806ad2f231069b86aff6a24a34070d1b/zalaksya.jpg' ,'tasest@example.com', 'hashed_password_456');
-
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('test12', 'test User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'test@eqexample.com', 'hashed_password_456');
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test6', 'test  User', 'https://cdn.intra.42.fr/users/9ae5b3303aaceb68d7a6e580c60545a4/yzoullik.jpg' ,'tesact@example.com', 'hashed_password_456');
 
 
-INSERT INTO users (username, fullname, profile_img ,email, password) 
-VALUES ('tesdt12', 'tedst User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tesdt@eqexample.com', 'hashed_password_456');
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test7', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tes zxt@example.com', 'hashed_password_456');
+
+
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test8', 'test  User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'teszxt@example.com', 'hashed_password_456');
+
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test9', 'test User', 'https://cdn.intra.42.fr/users/806ad2f231069b86aff6a24a34070d1b/zalaksya.jpg' ,'tasest@example.com', 'hashed_password_456');
+
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('test12', 'test User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'test@eqexample.com', 'hashed_password_456');
+
+
+-- INSERT INTO users (username, fullname, profile_img ,email, password) 
+-- VALUES ('tesdt12', 'tedst User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg' ,'tesdt@eqexample.com', 'hashed_password_456');
--- backend/modules/user.module.js
+++ backend/modules/user.module.js
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 
+
 export async function createUser(request, reply) {
   return {
     message: `User created`,
@@ -25,12 +26,63 @@ export async function aaa(request, reply) {
 }
 
 
-export async function getConversation(request , reply){
- console.log('Route params:', request.body);
-  reply.send([
-    {id: 1 , ms:'hello brother are you fine ', sender:'avatar'},
-    {id: 2 , ms:'hello brother are you fine ', sender:'savatae'},
-    {id: 2 , ms:'hello brother are you fine ', sender:'abechcha'},
-    {id: 2 , ms:'hello brother are you fine ', sender:'savatar'}
-]);
+
+
+export async function getConversationId(request, reply) {
+  const user = request.body.user;
+  const friend = request.body.friend;
+
+  const searchUser = `%,${user},%`;
+  const searchFriend = `%,${friend},%`;
+
+  try {
+    const query = request.server.db.prepare(
+      "SELECT conversation_id FROM room WHERE members LIKE ? AND members LIKE ?"
+    );
+    const result = query.get(searchUser, searchFriend);
+
+    if (result) {
+      return reply.send(result);
+
+    } else {
+      reply.code(200).send([]);
+    }
+  } catch (err) {
+    console.error("Database error:", err);
+    reply.code(500).send({ error: "Internal server error" });
+  }
+}
+
+
+
+export async function getMsgs (request , reply){
+  const id = request.body.id;
+  try{
+      const query = request.server.db.prepare("SELECT * FROM message WHERE conv_id = ? ORDER BY created_at ASC");
+      const messages = query.all(id);
+      return reply.send(messages);
+
+  }catch(err){
+    console.log(err)
+    reply.code(500).send({ error: "Internal server error" });
+  }
+
+}
+
+
+export async function sendMsg (request , reply){
+  console.log(request.body);
+
+  const {user , input , id} = request.body;
+
+  try{
+      const query = request.server.db.prepare("INSERT INTO message (conv_id, message, sender) VALUES (?, ?, ?)");
+      query.run(id, input, user);
+      reply.code(200);
+  }catch(err){
+    console.log(err)
+    reply.code(500).send({ error: "Internal server error" });
+  }
+
+
 }
