ef658695c346b206f4615bc995189449a6e4bfcd
mrmoughit <xblackhacker1@gmail.com>
2025-07-12 22:56:01 +0100

    m

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,4 +1,4 @@
-import { createUser, getUsers  , getConversationId ,aaa , sendMsg , getMsgs} from '../modules/user.module.js';
+import { createUser, getUsers  , getConversationId ,aaa , sendMsg , getMsgs , Xprank} from '../modules/user.module.js';
 
 export default async function routes(fastify, options) {
   fastify.get('/user', createUser);
@@ -7,5 +7,6 @@ export default async function routes(fastify, options) {
   fastify.post('/getConversationId', getConversationId);
   fastify.post('/sendMsg' , sendMsg);
   fastify.post('/getMsgs' , getMsgs);
+  fastify.get('/Xprank' , Xprank);
 
 }
