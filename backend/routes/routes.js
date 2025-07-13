1f0c25a683e3d899a6443a2ce7336dca147f0502
mrmoughit <xblackhacker1@gmail.com>
2025-07-13 12:05:47 +0100

    working in add friend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,12 +1,15 @@
-import { createUser, getUsers  , getConversationId ,aaa , sendMsg , getMsgs , Xprank} from '../modules/user.module.js';
+import {  getUsers  , getConversationId  , sendMsg , getMsgs , Xprank , sendNotification} from '../modules/user.module.js';
 
 export default async function routes(fastify, options) {
-  fastify.get('/user', createUser);
   fastify.get('/users', getUsers);
-  fastify.get('/', aaa);
   fastify.post('/getConversationId', getConversationId);
+  
   fastify.post('/sendMsg' , sendMsg);
   fastify.post('/getMsgs' , getMsgs);
+
   fastify.get('/Xprank' , Xprank);
+
+  fastify.post('/sendNotification' , sendNotification);
+  // fastify.get('/getNotification' , getNotification);
 
 }
