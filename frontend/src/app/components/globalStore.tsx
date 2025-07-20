36f22ed1107cf50c36a22428bb87d94a4035beee
mrmoughit <xblackhacker1@gmail.com>
2025-07-20 23:44:08 +0100

    confir request friend work need to craeet conversatino

--- frontend/src/app/components/globalStore.tsx
+++ frontend/src/app/components/globalStore.tsx
@@ -1,14 +1,34 @@
+"use client";
+
 import {create} from 'zustand';
 
 
-const globalStore = create((set . get ) =>({
+export const globalStore = create((set , get ) =>{
+
+
+    let name = localStorage.getItem('name');
+    set({ username: name });
+
+    return {
     socket : null,
     isConnect : false,
-    username : null , 
+    username : localStorage.getItem('name'), 
     token: null,
+
+
 
     connect: () =>{
         if (get().socket) return ;
-        
-    }
-}))
+        const ws = new WebSocket('ws://localhost:4444/ws');
+        ws.onopen = () => {
+            console.log("here is connect ");
+            ws.send(get().username);
+        };
+        set({socket: ws})
+
+        ws.onclose = () => {
+            console.log(' Disconnected');
+        };
+    },
+    };
+});
