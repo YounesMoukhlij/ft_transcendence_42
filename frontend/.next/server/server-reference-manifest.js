2586bb26951d26cd721545ad191be09be5abff34
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-07-22 14:35:29 +0100

    YM branch into main

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oCsIaojXWikeCI7olTpvxZV0B/nhCfYZEVL18CK1Vss=\"\n}"
--- frontend/.next/static/chunks/src_app_game_player2_page_tsx_b3af9b91._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_game_player2_page_tsx_af74308f._.js"
-],
-    source: "dynamic"
-});
