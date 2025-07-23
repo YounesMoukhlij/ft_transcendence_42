b6adf3e3f42c8e68a6059bc839cffdbe03000f4c
mrmoughit <xblackhacker1@gmail.com>
2025-07-23 10:53:38 +0100

    m

--- backend/init.sql
+++ backend/init.sql
@@ -109,9 +109,9 @@ VALUES
 ('tesdt12', 'test User', 'https://www.shutterstock.com/image-vector/black-woman-smiling-portrait-vector-600nw-2281497689.jpg', 'tesdt12@example.com', 'hashed_password_456', 4564);
 
 -- Insert a room
-INSERT INTO room (members, room_name, iconPad)
-VALUES (',abechcha,ziko,', 'Friends Chat', 'default_icon.png');
+-- INSERT INTO room (members, room_name, iconPad)
+-- VALUES (',abechcha,ziko,', 'Friends Chat', 'default_icon.png');
 
--- Insert a message linked to the existing room (id 1)
-INSERT INTO message (conv_id, message, sender)
-VALUES (1, 'Hello, how are you? I am Zakariya.', 'ziko');
+-- -- Insert a message linked to the existing room (id 1)
+-- INSERT INTO message (conv_id, message, sender)
+-- VALUES (1, 'Hello, how are you? I am Zakariya.', 'ziko');
